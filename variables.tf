9a0779b14236d15a9f82c6a34efee373378a7ccd
akastav <switch@bk.ru>
2021-03-22 19:12:57 +0300

    add last changes from main project

--- variables.tf
+++ variables.tf
@@ -1,20 +1,48 @@
-variable "zone_id" {
-  type        = string
-  description = "Default zone id for root domain"
-}
-
-variable "domain" {
+variable zone_id {
   type = string
 }
 
-variable "cluster_name" {
-  type        = string
-  default     = null
-  description = "A name of the Amazon EKS cluster"
+variable domain {
+  type = string
 }
 
-variable "tags" {
+variable cluster_name {
+  type        = string
+  description = "A name of the cluster"
+}
+
+variable tags {
   type        = map(string)
+  description = "A set of tags"
   default     = {}
-  description = "A tags for attaching to new created AWS resources"
+}
+
+variable mfa_configuration {
+  type        = string
+  description = "Turn MFA on or off. (Must be on of: ON, OFF, OPTIONAL)"
+  default     = "ON"
+}
+
+variable advanced_security_mode {
+  type        = string
+  description = "Sets Advanced Security for a user pool (OFF, AUDIT, ENFORCED)"
+  default     = "ENFORCED"
+}
+
+variable acm_arn {
+  type = string
+  description = "The ARN of an ACM certificate to attach to the Load Balancer"
+  default = ""
+}
+
+variable invite_template {
+  type        = map(string)
+  description = "A template for the invite email with credentials"
+  default = {
+    email_message = <<EOT
+Your Swiss Army Kube username is {username} and temporary password is {####}.
+EOT
+    email_subject = "Your Swiss Army Kube temporary password"
+    sms_message   = "Your Swiss Army Kube username is {username} and temporary password is {####}"
+  }
 }
